42e9ef1711e76f130b856194da48577fadc4cfc2
parbilla <parbilla@gmail.com>
2020-12-08 21:16:21 +0000

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            var = 'FizzBuzz'
+        elif i % 5 == 0:
+            var = 'Buzz'
+        elif i % 3 == 0:
+            var = 'Fizz'
+        else:
+            var = i
+        print('{} '.format(var), end='')
