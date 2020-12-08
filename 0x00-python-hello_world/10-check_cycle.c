53f2f51645be10b8aa09f1815df70a058c0f3dca
parbilla <parbilla@gmail.com>
2020-12-08 01:20:36 +0000

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,4 +9,20 @@
 
 int check_cycle(listint_t *list)
 {
-	
+	/* pointers to linked list, one moves one place, two moves two */
+	listint_t *one, *two;
+
+	/* point pointers to head*/
+	one = two = list;
+
+	/* move pointers */
+	while (one != NULL && two != NULL && two->next != NULL)
+	{
+		two = two->next->next;
+		one = one->next;
+		/* if there is a cycle, in sometime two will be equal to one*/
+		if (two == one)
+			return (1);
+	}
+	return (0);
+}
