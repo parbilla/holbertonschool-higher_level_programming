761ff07ea6a5222e1c13f087cac3924bc1bb272e
parbilla <parbilla@gmail.com>
2020-12-15 21:32:10 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == 0:
-        return None
+    if sentence:
+        return (len(sentence), sentence[0])
     else:
-        tuple = (len(sentence), sentence[0])
-    return tuple
+        return None
