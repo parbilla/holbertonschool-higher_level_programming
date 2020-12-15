864ba16b2d6f80298658a6dcf7a77b2fed05d60c
parbilla <parbilla@gmail.com>
2020-12-15 22:44:20 +0000

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            row = row.append(j)
+        print('{:d}'.format(row), end='') 
