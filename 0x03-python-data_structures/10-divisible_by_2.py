421631d95d07c64a0cece3116b1c8c6723562847
parbilla <parbilla@gmail.com>
2020-12-15 22:41:07 +0000

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         copy_list = my_list.copy()
-        for i in my_list:
+        for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
                 copy_list[i] = True
             else:
