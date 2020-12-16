082a66d81aa1f5af6afd9cabc8cb0afdfaa0af2b
parbilla <parbilla@gmail.com>
2020-12-16 15:10:05 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,5 +6,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) < 2:
         for i in range(len(tuple_b), 2):
             tuple_b = tuple_b + (0,)
-    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return res
