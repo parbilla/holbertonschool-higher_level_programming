44bdcc1ad7c923410c7056238749ffd6c038c2c2
parbilla <parbilla@gmail.com>
2020-12-17 21:02:09 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        a_dictionary = sorted(a_dictionary.items(), key=lambda x: x[1], \
-                              reverse=True)
-        res = next(iter(a_dictionary))
+        n_dict = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
+        res = next(iter(n_dict))
         return res[0]
     else:
         return None
