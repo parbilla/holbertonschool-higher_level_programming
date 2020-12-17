e5424afb8c3b321d9046c509611768196fb13286
parbilla <parbilla@gmail.com>
2020-12-17 16:38:59 +0000

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,4 @@ def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
         if my_list[i] == search:
             new_list[i] = replace
- #   new_list = my_list.replace(search, replace)
     return new_list
