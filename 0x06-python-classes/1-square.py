3d4e4c19fb52135e38bf074848e1ed11abc40dfd
parbilla <parbilla@gmail.com>
2021-01-18 19:07:47 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """New class"""
+    def __init__(self, size=0):
+        """Instantiation with size"""
+        self.__size = size
