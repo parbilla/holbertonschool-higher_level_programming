3cf635703b6b9de06d1bc5de4f7fa867f17a25de
parbilla <parbilla@gmail.com>
2021-01-19 22:25:58 +0000

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Instantiation with width and height private"""
 
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
