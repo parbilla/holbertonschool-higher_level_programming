cb1024c6836c641019f66140d87feef0cf2c467c
parbilla <parbilla@gmail.com>
2021-01-19 21:41:09 +0000

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Returns if object is an instance of a class inherited from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """True if it inherited from a class"""
+
+    if issubclass(obj.__class__, a_class):
+        return True
+    else:
+        return False
