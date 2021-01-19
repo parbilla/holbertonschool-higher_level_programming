e65d637f63b08d5997c9ffdeeefbec9722635748
parbilla <parbilla@gmail.com>
2021-01-19 01:41:36 +0000

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """New class"""
+    def __init__(self, size=0):
+        """Instantiation with size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Conditions of size value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Method that returns current square area"""
+        return self.__size * self.__size
