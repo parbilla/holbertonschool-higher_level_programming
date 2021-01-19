2f698db89b135428dd9e031d65efbf7f1cbb9007
parbilla <parbilla@gmail.com>
2021-01-19 23:07:09 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Print a list , sorted in ascending sort"""
+
+
+class MyList(list):
+    """Write a class that inherits from another"""
+
+    def print_sorted(self):
+        """Prints the list sorted by ascending sort"""
+
+        if issubclass(MyList, list):
+            print(sorted(self))
