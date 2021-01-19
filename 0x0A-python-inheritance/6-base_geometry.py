22febb1b6e4e15baee2c50fdb58df5cf53a5b62e
parbilla <parbilla@gmail.com>
2021-01-19 21:54:10 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write an empty class"""
+
+
+class BaseGeometry:
+    """New class"""
+
+    pass
+
+    def area(self):
+        """public instance method"""
+
+        raise Exception(" area() is not implemented")
