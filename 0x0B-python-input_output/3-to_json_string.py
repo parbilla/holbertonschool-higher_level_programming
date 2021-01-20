82d0f7e43dffccecabbb5673892d8a728b85668c
parbilla <parbilla@gmail.com>
2021-01-20 16:47:37 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that returns the JSON representation of an object (string)"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """JSON representation of an object"""
+
+    return json.dumps(my_obj)
