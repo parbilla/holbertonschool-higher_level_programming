6aa1d5bcdeaa0d68526947d6bbcb6f79dd454a5e
parbilla <parbilla@gmail.com>
2021-01-20 15:19:52 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Opens file and prints it"""
+
+    with open(filename, encoding='utf-8') as myFile:
+        print(myFile.read())
