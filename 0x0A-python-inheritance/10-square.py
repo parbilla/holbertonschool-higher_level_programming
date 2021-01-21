61300bb273145d4c4d3fc9c5e7f99b442d4b0c0b
parbilla <parbilla@gmail.com>
2021-01-21 14:52:41 +0000

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a class Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square inherited of Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Area method implemented"""
+        return self.__size ** 2
