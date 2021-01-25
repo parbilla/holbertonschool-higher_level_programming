797ce7fe1ec2700ab4235934bfd4ab1ef1875779
parbilla <parbilla@gmail.com>
2021-01-25 19:08:29 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,8 +5,38 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """set value"""
+        if type(size) is not int:
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}" .format(
             self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """assign an argument to each attribute"""
+        square_attrs = ["id", "size", "x", "y"]
+        if args is None or len(args) == 0:
+            for k, v in kwargs.items():
+                if k in square_attrs:
+                    setattr(self, k, v)
+        else:
+            for i in range (len(args)):
+                setattr(self, square_attrs[i], args[i])
+
+    def to_dictionary(self):
+        """return dictionary with values"""
+        square_dict = {'id': self.id, 'size': self.size,
+                       'x': self.x, 'y': self.y}
+        return square_dict
