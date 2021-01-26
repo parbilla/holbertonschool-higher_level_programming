ccaf2210a1bdbce5a48e8300edd3f63f4e32f666
parbilla <parbilla@gmail.com>
2021-01-26 15:09:50 +0000

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""Write the class Square that inherits from Rectangle"""
 from models.rectangle import Rectangle
 
-class Square(Rectangle):
 
+class Square(Rectangle):
+    """New class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """Get value"""
         return self.width
 
     @size.setter
     def size(self, size):
-        """set value"""
+        """Set value"""
         if type(size) is not int:
             raise TypeError("width must be an integer")
         if size <= 0:
@@ -21,11 +25,12 @@ class Square(Rectangle):
         self.height = size
 
     def __str__(self):
+        """Update the class Square by overriding the __str__ method"""
         return "[Square] ({}) {}/{} - {}" .format(
             self.id, self.x, self.y, self.size)
 
     def update(self, *args, **kwargs):
-        """assign an argument to each attribute"""
+        """Assign an argument to each attribute"""
         square_attrs = ["id", "size", "x", "y"]
         if args is None or len(args) == 0:
             for k, v in kwargs.items():
@@ -36,7 +41,7 @@ class Square(Rectangle):
                 setattr(self, square_attrs[i], args[i])
 
     def to_dictionary(self):
-        """return dictionary with values"""
+        """Return dictionary with values"""
         square_dict = {'id': self.id, 'size': self.size,
                        'x': self.x, 'y': self.y}
         return square_dict
