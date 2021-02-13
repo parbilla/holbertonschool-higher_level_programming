733c79cc9a03ded02cfdc7d42cd5c691c0aa049e
parbilla <parbilla@gmail.com>
2021-02-13 23:12:18 +0000

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
     def position(self, value):
         """Conditions of position value"""
         if type(value) is not tuple or len(value) != 2 or value[0] is not int \
-           or value[1] is not int or value[0] <= 0 or value[1] <= 0:
+           or value[1] is not int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
