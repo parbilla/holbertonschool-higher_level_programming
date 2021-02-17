163207488ff56a24d3e759c0eff4b038e88d6b4c
parbilla <parbilla@gmail.com>
2021-02-17 22:49:14 +0000

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,10 +10,12 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+    @property
     def width(self):
         """Get width"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """Set width"""
         if type(value) is not int:
@@ -22,10 +24,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """Get height"""
         return self.__height
 
+    @height.setter
     def height(self, value):
         """Set height"""
         if type(value) is not int:
