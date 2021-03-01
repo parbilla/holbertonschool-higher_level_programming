36d4b34c1a96e7eae0eccfacea93a4dd3ddbe795
parbilla <parbilla@gmail.com>
2021-03-01 20:05:46 +0000

    task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Displays the number of records with id = 89 in the table of the database in MySQL server
-SELECT id, COUNT('89') FROM first_table GROUP BY id;
+SELECT id, COUNT(89) FROM first_table GROUP BY id;
