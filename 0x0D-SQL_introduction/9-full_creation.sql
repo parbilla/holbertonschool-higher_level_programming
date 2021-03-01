a05b6ced0bdd5ae711637153b07fe8203d9d6915
parbilla <parbilla@gmail.com>
2021-03-01 20:31:50 +0000

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- Creates a table second_table in the current database in the MySQL server and add multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
