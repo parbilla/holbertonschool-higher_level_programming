7712f7af9295848b830c39678de7e38180585913
parbilla <parbilla@gmail.com>
2021-03-02 22:11:11 +0000

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Lists all the cities contained in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id;
