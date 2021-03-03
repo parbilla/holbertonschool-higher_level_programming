21f6208a6bd6fec24ccd988d39c2a01a01628c7e
parbilla <parbilla@gmail.com>
2021-03-03 15:30:32 +0000

    task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+
+SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows INNER JOIN tv_shows_genres ON tv_shows.id=tv_shows_genres.genre_id ORDER BY tv_shows.title, tv_shows_genres.genre_id
