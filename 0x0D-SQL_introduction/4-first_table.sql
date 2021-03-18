756c1c3b8c13eeb041e9699e8bf63c21bdaba46f
parbilla <parbilla@gmail.com>
2021-03-18 15:25:04 +0000

    task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Creates a table first_table in the current database in the MySQL server
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(25));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
