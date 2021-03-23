fabd3dcea367875ff2877368fe5f3fe5b8ac3569
parbilla <parbilla@gmail.com>
2021-03-23 15:01:19 +0000

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Define the class of a rectangle
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+        }
+    }
+};
