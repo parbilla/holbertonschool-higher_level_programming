e52699498838826c07a4c465d5a31b44541ca8d5
parbilla <parbilla@gmail.com>
2021-03-23 13:22:26 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 let second = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
-  args.sort(function(a,b) {return a -b;});
+  args.sort(function (a, b) { return a - b; });
   second = args[args.length - 2];
 }
 console.log(second);
