091e3a399fb22db077aec2fe994441d5097a4f64
parbilla <parbilla@gmail.com>
2021-03-23 17:39:46 +0000

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Defines a square and inherits from Square of 5-square.js
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
