2334d8d46cda17ffde88a7aa00ee2155b1700587
parbilla <parbilla@gmail.com>
2021-03-24 22:43:53 +0000

    task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that concats 2 files
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+const textA = fs.readFileSync(fileA, {encoding:'utf8', flag: 'r'});
+const textB = fs.readFileSync(fileB, {encoding:'utf8', flag: 'r'});
+fs.writeFileSync(fileC, textA + textB);
