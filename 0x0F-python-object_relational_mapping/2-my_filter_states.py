0f3df4ac78b4203d5388d0c4a09b5275d8033a71
parbilla <parbilla@gmail.com>
2021-03-27 21:19:03 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     """Obtengo un cursor para ejecutar las consultas"""
     cur = db.cursor()
     """Ejecuto consulta utilizando el cursor"""
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id"
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY states.id"
                 .format(argv[4]))
     rows = cur.fetchall()
     for i in rows:
-        print('{}'.format(i))
+        print(i)
     cur.close()
     db.close()
