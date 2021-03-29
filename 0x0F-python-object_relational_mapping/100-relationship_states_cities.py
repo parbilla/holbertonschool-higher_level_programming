356699560a76a256a7e08923e054b426c6fb4527
parbilla <parbilla@gmail.com>
2021-03-29 15:28:57 +0000

    task 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,14 +3,13 @@
     “San Francisco” from the database hbtn_0e_100_usa"""
 
 from sys import argv
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import State
-from relationship_city import City, Base
+from relationship_state import Base, State
+from relationship_city import City
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
