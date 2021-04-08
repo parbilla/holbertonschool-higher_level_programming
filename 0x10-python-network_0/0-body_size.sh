40a5405297e2c4beea173c4bf7a8bfcfc71b7d0d
parbilla <parbilla@gmail.com>
2021-04-08 18:37:42 +0000

    tasks 0-1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script takes in a URL, sends a GET request to the URL, and displays the size of the body of the response
+# Bash script takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
