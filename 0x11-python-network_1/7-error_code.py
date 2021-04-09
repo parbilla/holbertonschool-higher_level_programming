b11fa37359fc92804f84797e856ae098195f8efb
parbilla <parbilla@gmail.com>
2021-04-09 23:41:44 +0000

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ from sys import argv
 if __name__ == "__main__":
     r = requests.get(argv[1])
     if r.status_code == requests.codes.ok:
-        print(r)
+        print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
