d87ba711bfb8ef6f1ece0f5a949bca52659c8ae3
parbilla <parbilla@gmail.com>
2021-04-09 23:22:38 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Takes in a URL and an email, sends a POST request to the passed URL\
+ with the email as a parameter, and displays the body of the response"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
