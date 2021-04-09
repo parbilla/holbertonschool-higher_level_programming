2b7b6a249ea4d70ec4a3b98e5834de41b420d3d1
parbilla <parbilla@gmail.com>
2021-04-09 22:47:02 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and\
+displays the value of the X-Request-Id"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
