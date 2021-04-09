d1c6b2c7b00cf4b68077b6024d2a523e7a487d73
parbilla <parbilla@gmail.com>
2021-04-09 20:39:59 +0000

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response"""
+"""Takes in a URL and an email, sends a POST request to the passed URL\
+ with the email as a parameter, and displays the body of the response"""
 import urllib.request
 import urllib.parse
 from sys import argv
@@ -7,10 +8,10 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    values = {'email' : argv[2]}
+    values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
-    req= urllib.request.Request(url, data)
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         mail = response.read()
         print("Your email is: {}".format(argv[2]))
