64665596ac6b6c572c8104153934b906cad52c5a
parbilla <parbilla@gmail.com>
2021-04-10 01:20:55 +0000

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,8 +5,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    r = requests.get('https://api.github.com/user', auth=('username', 'password'))
+    user = argv[1]
+    pass = argv[2]
+    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
     user = r.json()
     print(user.get('id'))
