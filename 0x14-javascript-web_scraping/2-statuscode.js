7c9245351c5a32b5cbb8fe79a960df7a8808fc17
parbilla <parbilla@gmail.com>
2021-05-04 15:26:21 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 request.get(process.argv[2])
   .on('response', function (response) {
-    console.log("code: " + response.statusCode);
+    console.log('code: ' + response.statusCode);
   });
