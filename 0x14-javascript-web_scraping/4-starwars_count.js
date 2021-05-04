8b5f5a2aab3b8664dc06efa8ffb0d5871afcf269
parbilla <parbilla@gmail.com>
2021-05-04 18:18:57 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const urlId = process.argv[2] + '/?format=json';
-let cont = 0;
-request.get(urlId, function (response, body) {
-  const filmList = (JSON.parse(body.body).results);
-  for (const film in filmList) {
-    for (const i in filmList[film].characters) {
-      if (filmList[film].characters[i].includes('18')) {
-        cont++;
-      }
+const urlId = process.argv[2];
+request(urlId, function (err, response, body) {
+    if (err) {
+	console.log(err);
+    } else {
+	const filmList = JSON.parse(body).results;
+	let cont = 0;
+	for (const film in filmList) {
+	    for (const i in filmList[film].characters) {
+		if (filmList[film].characters[i].includes('18')) {
+		    cont++;
+		}
+	    }
+	}
+	console.log(cont);
     }
-  }
-  console.log(cont);
 });
