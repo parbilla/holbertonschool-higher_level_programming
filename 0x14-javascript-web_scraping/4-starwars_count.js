1489837c791b82608529c3d5a01f918f462e63c2
parbilla <parbilla@gmail.com>
2021-05-04 18:08:59 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,11 @@
 const request = require('request');
 const urlId = process.argv[2] + '/?format=json';
 let cont = 0;
-const name = 'https://swapi-api.hbtn.io/api/people/18/';
 request.get(urlId, function (response, body) {
   const filmList = (JSON.parse(body.body).results);
-  for (let film in filmList) {
-    for (let i in filmList[film].characters) {
-      if (filmList[film].characters[i] === name) {
+  for (const film in filmList) {
+    for (const i in filmList[film].characters) {
+      if (filmList[film].characters[i].includes('18')) {
         cont++;
       }
     }
