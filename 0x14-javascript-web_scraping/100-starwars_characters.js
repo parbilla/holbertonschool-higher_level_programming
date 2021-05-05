04be3e8d994f2ccadc8a7918d434f08747b6c0e0
parbilla <parbilla@gmail.com>
2021-05-05 14:38:48 +0000

    task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const reqChar = require('request');
+const urlId = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+let charList = [];
+request(urlId, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    charList = JSON.parse(body).characters;
+    for (const i in charList) {
+      reqChar(charList[i], function (err, response, body) {
+        if (err) {
+	    console.log(err);
+        } else {
+	    console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
