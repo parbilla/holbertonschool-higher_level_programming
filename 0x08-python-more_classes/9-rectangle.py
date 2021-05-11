c303bfe42fb51cef381088bf23ee0839b9cb651c
parbilla <parbilla@gmail.com>
2021-05-11 19:33:42 +0000

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle:
     def __repr__(self):
         """Returns a string representation of the rectangle
         to be able to recreate a new instance by using eval()"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         """Prints message when an instance is deleted"""
