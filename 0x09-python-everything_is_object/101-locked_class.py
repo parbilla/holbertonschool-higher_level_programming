0f839aefeb2711613cbb18c1ce5eea00e39d7c85
parbilla <parbilla@gmail.com>
2021-06-25 20:38:57 +0000

    task 31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This class has no class or object attribute"""
+
+
 class LockedClass:
     """Only instances a new attribute if it is called first_name"""
     __slots__ = ["first_name"]
