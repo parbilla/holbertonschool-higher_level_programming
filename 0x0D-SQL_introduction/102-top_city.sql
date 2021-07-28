53a9aa4fa8472259e0e9997513eb707780afb668
parbilla <parbilla@gmail.com>
2021-07-28 17:36:24 +0000

    task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(value) 'avg_temp' FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
