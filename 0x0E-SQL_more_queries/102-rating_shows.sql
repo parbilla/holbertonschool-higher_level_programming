d7350d154253be431fe8ba29abc3d7aa988c0d32
parbilla <parbilla@gmail.com>
2021-07-30 18:52:50 +0000

    task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 -- Lists all shows from the database hbtn_0d_tvshows_rate by their rating
 
-SELECT ts.title, SUM(tr.rate)
+SELECT ts.title, sum(tr.rate)
 FROM tv_shows ts
 INNER JOIN tv_show_ratings tr
 ON ts.id=tr.show_id
 GROUP BY ts.title
-ORDER BY SUM(tr.rate) DESC;
+ORDER BY sum(tr.rate) DESC;
